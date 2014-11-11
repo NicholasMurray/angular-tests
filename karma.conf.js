ea771496411c9126eb1acad225634d86627850a7
Nicholas Murray <murray.nicholas@gmail.com>
2014-11-11 23:48:24 +0000

    Added Istanbul Code Coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,8 @@
-// Karma configuration
-// Generated on Wed Nov 05 2014 21:56:03 GMT+0000 (GMT)
-
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    autoWatch: true,
     frameworks: ['jasmine'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -23,45 +14,17 @@ module.exports = function(config) {
         'tests/*.js'
     ],
 
-
-    preprocessors: {
-      'app/*.js': 'coverage'
-    },
-
-    // list of files to exclude
-    exclude: [
-
-    ],
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress', 'coverage'],
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    //browsers: ['PhantomJS'],
     browsers: ['Chrome'],
 
+    reporters: ['progress', 'coverage'],
+    preprocessors: { 'app/*.js': ['coverage'] },
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    },
+
+    singleRun: true
   });
 };
